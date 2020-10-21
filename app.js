b94d883425af8bcf5fccb4ed24f1f8561538496f
Kareem Gurung <kareemgurung@Kareems-MacBook-Pro.local>
2020-10-21 14:29:40 +0100

    Create sign up page using bootstrap theme

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const request = require("request");
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
